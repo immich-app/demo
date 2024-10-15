7412314479fc36dab59c5692dc6047682464bf88
bo0tzz <git@bo0tzz.me>
2024-10-15 16:53:36 +0200

    chore: update port mapping (#28)

--- demo-restart.sh
+++ demo-restart.sh
@@ -11,9 +11,9 @@ docker compose pull
 docker compose down -v
 docker compose up -d immich-server
 
-until curl 127.0.0.1:3001/api/server-info/ping; do sleep 1; done
+until curl 127.0.0.1:2283/api/server-info/ping; do sleep 1; done
 
-curl --location --request POST '127.0.0.1:3001/api/auth/admin-sign-up' \
+curl --location --request POST '127.0.0.1:2283/api/auth/admin-sign-up' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "email": "demo@immich.app",
@@ -25,12 +25,12 @@ docker compose up -d
 
 sudo iptables -D INPUT -p tcp --dport 80 -j DROP
 
-accessToken=$(curl -d '{"email": "demo@immich.app", "password": "demo"}' -H "Content-Type: application/json" -X POST 127.0.0.1:3001/api/auth/login | jq -r '.accessToken')
+accessToken=$(curl -d '{"email": "demo@immich.app", "password": "demo"}' -H "Content-Type: application/json" -X POST 127.0.0.1:2283/api/auth/login | jq -r '.accessToken')
 header="Authorization: Bearer ${accessToken}"
-apiKey=`curl -d '{"name": "Demo controller", "permissions": ["all"]}' -H "Content-Type: application/json" -H "$header" -X POST 127.0.0.1:3001/api/api-keys | jq -r '.secret'`
+apiKey=`curl -d '{"name": "Demo controller", "permissions": ["all"]}' -H "Content-Type: application/json" -H "$header" -X POST 127.0.0.1:2283/api/api-keys | jq -r '.secret'`
 
 docker run --rm -v /home/ubuntu/demo/images:/import --network demo_default \
-    -e IMMICH_INSTANCE_URL="http://immich-server:3001/api" -e IMMICH_API_KEY="$apiKey" \
+    -e IMMICH_INSTANCE_URL="http://immich-server:2283/api" -e IMMICH_API_KEY="$apiKey" \
     --pull always ghcr.io/immich-app/immich-cli:latest \
     upload -r -h /import
 docker system prune -f
